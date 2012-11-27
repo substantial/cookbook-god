8ae80cfbad012113bb123c8f0098e55c82c3a4a3
Aaron Jensen <aaronjensen@gmail.com>
2012-11-27 06:33:26 -0800

    support scientific

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,10 @@ maintainer_email "allen@substantial.com"
 license          "Apache 2.0"
 description      "Installs/Configures god"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.2.0"
 
 recipe "god", "Installs god"
 
-%w{fedora centos rhel ubuntu debian}.each do |os|
+%w{fedora centos rhel ubuntu debian scientific}.each do |os|
   supports os
 end
