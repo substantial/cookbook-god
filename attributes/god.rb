e138f056b962e1f92b548d4f03ea7e3783c966a7
Aaron Jensen <aaronjensen@gmail.com>
2012-11-26 23:59:50 -0800

    Some changes to the god recipe
    * Install to a specifiable ruby/gemset
    * Put config in a global path /etc/god by default
    * Starts god on boot

---
+++ attributes/god.rb
@@ -0,0 +1,1 @@
+default['god']['config_path'] = '/etc/god'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,52 @@
-rvm_gem "god"
+ruby = node['god']['ruby'] || node['rvm']['default_ruby']
+gemset = node['god']['gemset'] || 'god'
+ruby_string = "#{ruby}@#{gemset}"
+config_path = node['god']['config_path']
+
+rvm_gemset gemset do
+  ruby_string ruby
+end
+
+rvm_gem "god" do
+  ruby_string ruby_string
+end
+
+rvm_wrapper "god" do
+  ruby_string ruby_string
+  binary "god"
+end
+
+[config_path, "#{config_path}/conf.d"].each do |directory|
+  directory directory do
+    recursive true
+    owner "root"
+    group "root"
+    mode 0755
+  end
+end
+
+template "#{config_path}/conf.god" do
+  source "conf.god.erb"
+  owner "root"
+  group "root"
+  mode 0755
+  variables(
+    :config_paths => "#{config_path}/conf.d/*.god",
+  )
+end
+
+template "/etc/init.d/god" do
+  source "god_init.erb"
+  owner "root"
+  group "root"
+  mode 0755
+  variables(
+    :god_config_path => "#{config_path}/conf.god",
+    :god_path => "#{node['rvm']['root_path']}/bin/god_god"
+  )
+end
+
+service "god" do
+  supports :start => true, :stop => true, :restart => true, :status => true
+  action [:enable, :start]
+end
